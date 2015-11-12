72170045b32ce250dab69a5cf853ea7cb7516b08
shenjun <shenj@chemao.com.cn>
2015-11-12 14:30:01 +0800

    fixed problem

--- VegetaExample/vegeta/src/main/java/com/android/vageta/util/RandomUtil.java
+++ VegetaExample/vegeta/src/main/java/com/android/vageta/util/RandomUtil.java
@@ -8,21 +8,28 @@ import java.util.Random;
  */
 public class RandomUtil {
 
-        /**
-         * 产生随机数
-         *
-         * @param length
-         * @return
-         */
-        public static String getRandomString(int length) { //length表示生成字符串的长度
-            String base = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKMLNOPQRSTUVWXYZ";
-            Random random = new Random();
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < length; i++) {
-                int number = random.nextInt(base.length());
-                sb.append(base.charAt(number));
-            }
-            return sb.toString();
+
+    public static final String UPPER_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String LOWER_CASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    public static final String NUMBERS = "0123456789";
+    public static final String TOTAL_CHARS = UPPER_CASE_LETTERS + LOWER_CASE_LETTERS + NUMBERS;
+
+
+    /**
+     * 产生随机数
+     *
+     * @param length  生成字符串的长度
+     * @return
+     */
+    public static String getRandomString(int length) {
+        int cahr_len = TOTAL_CHARS.length();
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(cahr_len);
+            sb.append(TOTAL_CHARS.charAt(number));
         }
+        return sb.toString();
+    }
 
 }
